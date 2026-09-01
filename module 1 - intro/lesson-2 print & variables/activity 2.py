bb7e3a1f46cd8646157ce3895dcebb467e8b8a7b
Urooj Abdul Rahim <m.a.rahim001@gmail.com>
2026-09-01 20:40:59 +0300

    finished lesson 2 activity 2

---
+++ module 1 - intro/lesson-2 print & variables/activity 2.py
@@ -0,0 +1,7 @@
+a=int(input("Enter your no:"))
+b=56
+sum=a+b
+print("The sum of",a, "and", b, "is qual to", sum)
+print(f"The sum of {a} and {b} is equal to {sum}") #F string- f stands for formatted (replaces the variable w the values.)
+import keyword
+print(keyword.kwlist)
