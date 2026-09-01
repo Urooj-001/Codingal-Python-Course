2fe7252fa41d2f69cf997d768ca167e794684025
Urooj Abdul Rahim <m.a.rahim001@gmail.com>
2026-09-01 21:19:06 +0300

    Completed B-day hw.

---
+++ module 1 - intro/lesson-2 print & variables/Homework-Lesson 2.py
@@ -0,0 +1,10 @@
+a = "30/12/2010"
+b = "2/4/2010"
+c = "16/7/2011"
+d = "18/10/2010"
+e = "21/11/2008"
+print(f"Sumaira's birthday is on {a}.. HAPPIEST B-DAAYYY TO YOUUUUU!!")
+print(f"Ray's birthday is on {b}.. HAAPPYY BIRTHDAAAYYY MAY GOD BLESS YOU<3.")
+print(f"Mehwish's brithday is on {c}. WOOHOOO I WISH U A VERYY HAPPY BDAYYY.")
+print(f"Sukaina's birhtday is on {d}.. haappyy bdaaayy girlieee :)")
+print(f"Maha's birthday is on {e}.. HAAPPYY B-DAAAY SISTAAA.")
