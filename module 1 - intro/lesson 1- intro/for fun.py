ca1382ffa5acdbf5206427dda38a81e0c23465f1
Urooj Abdul Rahim <m.a.rahim001@gmail.com>
2026-09-01 20:21:28 +0300

    Finished lesson 2 acivity 1

---
+++ module 1 - intro/lesson 1- intro/for fun.py
@@ -0,0 +1,6 @@
+while True:
+ age = int(input("Enter your age please:"))
+ if age >= 18:
+    print("Your eligible for voting!")
+ else:
+    print("Sorry your not eligible to vote!")
---
+++ module 1 - intro/lesson-2 print & variables/activity 1.py
@@ -0,0 +1,19 @@
+print("Today am learning about print.")
+#i can print numbers without quotes if required.
+print(1234)
+print(54.16)
+print(100+200)
+print("100" + "200") #Join strings together with a + sign - CONCATENATION
+print("Line 1")
+print()
+print("Line 2")
+print()
+print("Line 3") #By default print always starts at a new line.
+print("Line 4\n\n\n\n")
+print("Line 5\n")
+print("Line 6\n")
+print("Today is lesson",2)
+print("The sum of 19 + 45 is",19+45,"That is addition!")
+print("Urooj", end="  ") #end , its ends the statement (replacing the new line)
+print("Abdul", end="  ")
+print("Rahim")
