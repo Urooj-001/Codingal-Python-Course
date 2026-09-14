b583bc84692722c7e40b8a7911c50b4c6b90b059
Urooj Abdul Rahim <m.a.rahim001@gmail.com>
2026-09-14 20:15:55 +0300

    A2 M2 L2

---
+++ module 2- loops/Lesson-2 For Loop/Activity-2.py
@@ -0,0 +1,5 @@
+#print no from 10 to 1 in reverse order 
+
+for i in range(10,0,-2): #step- gives the direction in which range should go but also give no of steps. (like addn and subraction to the first no.)
+    print(i)
+#by default step is always 1. 
