80f1b01eebdb831ba32d77f08f741edd9bad879d
Urooj Abdul Rahim <m.a.rahim001@gmail.com>
2026-09-20 19:49:52 +0300

    M2 L1 HOEMWORK DONE

--- module 1 - intro/lesson 1- intro/for fun.py
+++ module 1 - intro/lesson 1- intro/for fun.py
@@ -1,6 +1,13 @@
-while True:
- age = int(input("Enter your age please:"))
- if age >= 18:
-    print("Your eligible for voting!")
- else:
-    print("Sorry your not eligible to vote!")
+r=int(input("Enter your radius:"))
+print()
+print("What would u like to find? \n 1.Area \n 2.Perimeter")
+print()
+choice = int(input("Enter a choice (1/2):"))
+if choice == 1:
+    Area = 3.14 * r ** 2
+    print("Area is", Area)
+elif choice == 2:
+    Perimeter = 2 * 3.14 * r
+    print("Perimeter is", Perimeter )
+else:
+    print("Not found.")
---
+++ module 2- loops/Lesson-1 Nested conditions/Homework.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age:"))
+if age >= 10:
+    if age <= 20:
+        print("between 10 - 20")
+    else:
+        print("above 20")
+else:
+    print("Special age..lol")
