ba2479efd22212d2caf02c82ad6765ed3365a053
Urooj Abdul Rahim <m.a.rahim001@gmail.com>
2026-09-22 20:32:04 +0300

    M2 L6 A1 done

---
+++ module 2- loops/Lesson-6 Intro to Turtle!!/Activity-1.py
@@ -0,0 +1,25 @@
+import turtle
+turtle.Screen().bgcolor("#F08080")  #hashtag gives color (Hexcode color)
+turtle.Screen().setup(500 , 500)
+pen = turtle.Turtle()
+# pen.forward(100)
+# pen.right(90)
+# pen.forward(100)
+# pen.right(90)
+# pen.forward(100)
+# pen.right(90)
+# pen.forward
+for i in range(4):
+    pen.forward(100)
+    pen.right(90)
+
+
+
+
+
+
+
+
+
+
+turtle.done()
---
+++ module 2- loops/Lesson-6 Intro to Turtle!!/Activity-2.py
@@ -0,0 +1,30 @@
+import turtle
+turtle.Screen().bgcolor("#F08080")  #hashtag gives color (Hexcode color)
+turtle.Screen().setup(500 , 500)
+pen = turtle.Turtle()
+pen.color("#2E3F2C")
+pen.forward(100)
+pen.left(120)
+pen.forward(100)
+pen.left(120)
+pen.forward(100)
+pen.right(150)
+
+pen.penup()     #penup stops the line from drawn..pen picked up from the paper 
+pen.forward(60)
+pen.right(90)
+pen.pendown()
+pen.forward(100)
+pen.right(120)
+pen.forward(100)
+pen.right(120)
+pen.forward(100)
+pen.right(150)
+
+
+
+
+
+
+
+turtle.done()
