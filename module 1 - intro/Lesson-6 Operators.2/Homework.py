cf1125072a1933645bc6fb502043bf3b5f335d83
Urooj Abdul Rahim <m.a.rahim001@gmail.com>
2026-09-22 19:41:55 +0300

    M1 L6 HOMEOWRK-DONE

--- module 1 - intro/Lesson-6 Operators.2/Homework.py
+++ module 1 - intro/Lesson-6 Operators.2/Homework.py
@@ -1,2 +1,7 @@
 charc = input("Enter your character: ")
-charc = charc
+if charc >= "a" and charc <= "z":
+    print("Your character is a alphabet!")
+elif charc >= "A" and charc <= "Z":
+    print("Your character is a alphabet!")
+else:
+    print("Your character is not a alphabet.")
