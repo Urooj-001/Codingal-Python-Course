8b233b09a04cfb014db4fa9fc046a63da7e74019
Urooj Abdul Rahim <m.a.rahim001@gmail.com>
2026-09-24 22:51:01 +0300

    M2 L4 Homeowrk done

--- module 2- loops/Lesson-4 Nested Loop/homework.py
+++ module 2- loops/Lesson-4 Nested Loop/homework.py
@@ -1,14 +1,15 @@
 # Take a variable num and assign any positive number to it
-
-# Create an empty-string variable - binary_digits. This will hold the final "answer"
-
+num = int(input("Enter a number to convert it into a binary digit: "))
+binary_digits = ""
 # Run while loop - as long as n does not go below 0
-    # Get remainder (by 2)
-    # Add the remainder to the answer variable created above
-    # Floor division to reduce n - What will you divide by?
+n= num
+while n>0:
+    remainder = n % 2
+    binary_digits = binary_digits + str(remainder)
+    n = n//2
+binary_digits = binary_digits[ : : -1]
+print("The binary number for", num, "is", binary_digits)
 
-# Reverse the string to get the correct binary representation
 
-# Print the answer in this format - "The binary number for 25 is 11001"
 
-#Note: What if num is 0 - does the above process work?
+
